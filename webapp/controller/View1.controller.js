2b59dad9b4044e258113f19fbf7416da4e41a635
I320119 <vishwanath.s@sap.com>
2018-07-07 10:09:50 +0000

    initial commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sapui5_gittest.sapui5_gittest.controller.View1", {
+
+	});
+});
